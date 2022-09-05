e32e42a838ba2b2daeb488f9de5c3ad18a14dd1d
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-09-06 00:32:03 +0300

    infiniteScroll added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { renderGalleryMarkup } from './templates/renderGalleryMarkup';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix';
 
 const refs = {
     form: document.querySelector('#search-form'),
@@ -13,28 +14,36 @@ const refs = {
 let querry = '';
 let page;
 let lightbox;
-const notifyParams = {
+const notifyOptions = {
                 position: 'center-top',
                 distance: '55px',
                 showOnlyTheLastOne: true,
 }
 
+const infiniteScrollOptions = {    
+    rootMargin: '0px',
+    threshold: 1.0
+}
+
+const observer = new IntersectionObserver(scrollCallBack, infiniteScrollOptions);
+let target;
+
 refs.form.addEventListener('submit', onSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSubmit(evt) {
     evt.preventDefault();
     
     if (evt.currentTarget.elements.searchQuery.value.trim() === '') {        
 
-        Notify.warning("Please, enter your search query.", notifyParams)
+        Notify.warning("Please, enter your search query.", notifyOptions)
 
         return
     }
 
     if (evt.currentTarget.elements.searchQuery.value.trim() === querry) {        
 
-        Notify.warning("Please, try another search query.", notifyParams)
+        Notify.warning("Please, try another search query.", notifyOptions)
 
         return
     }
@@ -42,7 +51,7 @@ function onSubmit(evt) {
     querry = evt.currentTarget.elements.searchQuery.value.trim();
     page = 1;
     refs.gallery.innerHTML = '';
-    refs.loadMoreBtn.classList.add('visually-hidden');
+    // refs.loadMoreBtn.classList.add('visually-hidden');
 
 
     getQuerry(querry, page)
@@ -50,16 +59,23 @@ function onSubmit(evt) {
 
             if (response.data.hits.length === 0) {
                 Notify.warning("Sorry, there are no images matching your search query. Please try again.",
-                    notifyParams);
+                    notifyOptions);
                 return
             }
 
             renderMarkup(response);
-
-            Notify.success(`Hooray! We found ${response.data.totalHits} images.`, notifyParams)
             
-            if (response.data.totalHits > response.config.params.per_page) {
-                refs.loadMoreBtn.classList.remove('visually-hidden');
+            target = refs.gallery.lastElementChild;
+            observer.observe(target);
+
+            Notify.success(`Hooray! We found ${response.data.totalHits} images.`, notifyOptions)
+            
+            // if (response.data.totalHits > response.config.params.per_page) {
+            //     refs.loadMoreBtn.classList.remove('visually-hidden');                
+            // }
+
+            if (response.data.totalHits < response.config.params.per_page) {                
+                observer.disconnect();
             }
 
             const { height: cardHeight } = refs.form.getBoundingClientRect();
@@ -74,28 +90,47 @@ function onSubmit(evt) {
         .catch(onQuerryError)
 }
 
+function scrollCallBack(entries, observer) {
+
+    // const arr = observer.takeRecords();
+    // console.log(arr);
+    // console.log(entries[0]);
+    // console.log(entries[0].isIntersecting);    
+    // if (entries[0].intersectionRatio <= 0) return;
+    
+
+    if (entries[0].isIntersecting) {
+        onLoadMore();
+    };
+
+    target = refs.gallery.lastElementChild;
+    observer.observe(target);
+}
+
 function onLoadMore() {
     
     page += 1;
     getQuerry(querry, page)        
         .then(response => {            
             if (page > response.data.totalHits/response.config.params.per_page) {
-                refs.loadMoreBtn.classList.add('visually-hidden');
-                
+                // refs.loadMoreBtn.classList.add('visually-hidden');
+                // observer.unobserve(target);
+
                 Notify.warning("We're sorry, but you've reached the end of search results.",
-                    notifyParams)
+                    notifyOptions)                
+                
+                observer.disconnect();
             }
-            renderMarkup(response);        
+            renderMarkup(response);            
             
-            lightbox.refresh();
-        })
+            lightbox.refresh();            
+        })    
 }
 
 function renderMarkup(gallery) {
     refs.gallery.insertAdjacentHTML('beforeend', renderGalleryMarkup(gallery.data.hits));   
 }
 
-
 function onQuerryError() {    
-    Notify.failure("Woops, something went wrong. Please, keep calm and do your job", notifyParams)    
+    Notify.failure("Woops, something went wrong. Please, keep calm and do your job", notifyOptions)    
 }
